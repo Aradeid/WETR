8cc7be7d0fc4e821fb6c5e5dbc35f5d19d3cbe85
Mihail Curmei <mcurmei@mail.ru>
2018-12-17 03:39:10 +0100

    Updated domain to the new data modell

--- SWK5.WETR/SWK5.WETR/Entities/Measurement.cs
+++ SWK5.WETR/SWK5.WETR/Entities/Measurement.cs
@@ -7,25 +7,13 @@ using System.Threading.Tasks;
 namespace SWK5.WETR.Entities
 {
     public class Measurement : BaseEntity
-    {       
-        private double airTemperature { get; }
-        private double airPressure { get; }
-        private int precipitation { get; }
-        private double humidity { get; }
-        private double windSpeed { get; }
-        private int windDirection { get; } // in degreees
+    {
         private double value { get; } // measured in measurementUnit
         private double measurementUnitId { get; } // FK on UnitOfMeasurement
         private DateTime timeStamp { get; }
         private double stationId { get; } // FK on Station
-        public Measurement(double airTemperature, double airPressure, int precipitation, double humidity, double windSpeed, int windDirection, double value, double measurementUnitId, DateTime timeStamp, double stationId, int id = -1) : base(id)
+        public Measurement(double value, double measurementUnitId, DateTime timeStamp, double stationId, int id = -1) : base(id)
         {
-            this.airTemperature = airTemperature;
-            this.airPressure = airPressure;
-            this.precipitation = precipitation;
-            this.humidity = humidity;
-            this.windSpeed = windSpeed;
-            this.windDirection = windDirection;
             this.value = value;
             this.measurementUnitId = measurementUnitId;
             this.timeStamp = timeStamp;
@@ -33,12 +21,6 @@ namespace SWK5.WETR.Entities
         }
 		public override string ToString() {
 			return base.ToString()
-				+ ", airTemperature[" + airTemperature + "]"
-				+ ", airPressure[" + airPressure + "]"
-				+ ", precipitation[" + precipitation + "]"
-				+ ", humidity[" + humidity + "]"
-				+ ", windSpeed[" + windSpeed + "]"
-				+ ", windDirection[" + windDirection + "]"
 				+ ", value[" + value + "]"
 				+ ", measurementUnitId[" + measurementUnitId + "]"
 				+ ", timeStamp[" + timeStamp + "]"
--- SWK5.WETR/SWK5.WETR/Entities/UnitOfMeasurement.cs
+++ SWK5.WETR/SWK5.WETR/Entities/UnitOfMeasurement.cs
@@ -9,14 +9,17 @@ namespace SWK5.WETR.Entities
     public class UnitOfMeasurement : BaseEntity // class might be deleted later, as it seems to only be needed internally
     {
         private string unit { get; }
-        public UnitOfMeasurement(string unit, int id = -1) : base(id)
+        private string description { get; }
+        public UnitOfMeasurement(string unit, string description, int id = -1) : base(id)
         {
             this.unit = unit;
+            this.description = description;
         }
 		public override string ToString() {
 			return base.ToString()
 				+ ", unit[" + unit + "]"
-				;
+                + ", description[" + description + "]"
+                ;
 		}
 	}
 }
