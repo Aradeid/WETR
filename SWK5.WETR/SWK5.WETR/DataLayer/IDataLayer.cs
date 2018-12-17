d63c69c19501dc87a2cf016b372abce70e2840bd
Mihail Curmei <mcurmei@mail.ru>
2018-12-17 04:01:20 +0100

    Added DataLayer interface

---
+++ SWK5.WETR/SWK5.WETR/DataLayer/IDataLayer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SWK5.WETR.Entities;
+
+namespace SWK5.WETR.DataLayer {
+    interface IDataLayer {
+        #region user
+        ICollection<User> getAllUsers();
+        User getUserById(int id);
+        void addUser(User user);
+        void updateUser(User user);
+        void deleteUser(User user);
+        #endregion
+
+        #region station
+        ICollection<Station> getAllStations();
+        Station getStationById(int id);
+        void addStation(Station station);
+        void updateStation(Station station);
+        void deleteStation(Station station);
+        #endregion
+
+        #region location
+        ICollection<Location> getAllLocations();
+        Location getLocationById(int id);
+        void addLocation(Location location);
+        void updateLocation(Location location);
+        void deleteLocation(Location location);
+        #endregion
+
+        #region unitOfMeasurement
+        ICollection<UnitOfMeasurement> getAllUnitOfMeanurements();
+        UnitOfMeasurement getUnitOfMeanurementById(int id);
+        void addUnitOfMeanurement(UnitOfMeasurement unit);
+        void updateUnitOfMeanurement(UnitOfMeasurement unit);
+        void deleteUnitOfMeanurement(UnitOfMeasurement unit);
+        #endregion
+
+        #region measurement
+        ICollection<Measurement> getAllMeasurements();
+        Measurement getMeasurementById(int id);
+        Measurement getMeasurementByUnit(UnitOfMeasurement unit);
+        ICollection<Measurement> getMeasurementsBetween(DateTime startTime, DateTime endTime);
+        void addMeasurement(Measurement measurement);
+        void updateMeasurement(Measurement measurement);
+        void deleteMeasurement(Measurement measurement);
+        #endregion
+    }
+}
