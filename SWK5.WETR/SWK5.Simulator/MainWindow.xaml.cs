50c85f2c60c60f6182c564a7f83b3d231276885d
Mihail Curmei <mcurmei@mail.ru>
2018-12-22 21:17:44 +0100

    Pushed unfinished simulator experimental code

---
+++ SWK5.WETR/SWK5.Simulator/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SWK5.Simulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TemperatureViewModel tvm = new TemperatureViewModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new TemperatureViewModel();
+        }
+
+        private void ReadButton_Click(object sender, RoutedEventArgs e)
+        {
+            tvm.ReadCommand();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            tvm.StopCommand();
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            tvm.ClearCommand();
+        }
+    }
+}
---
+++ SWK5.WETR/SWK5.Simulator/TemperatureViewModel.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SWK5.Simulator
+{
+    public class TemperatureViewModel : INotifyPropertyChanged
+    {
+        private double _trend;
+        private double _count;
+        private double _currentLecture;
+        private bool _isHot;
+
+        public TemperatureViewModel()
+        {
+            
+        }
+
+        public bool IsReading { get; set; } = false;
+        
+        public double Count
+        {
+            get { return _count; }
+            set
+            {
+                _count = value;
+                OnPropertyChanged("Count");
+            }
+        }
+
+        public double CurrentLecture
+        {
+            get { return _currentLecture; }
+            set
+            {
+                _currentLecture = value;
+                OnPropertyChanged("CurrentLecture");
+            }
+        }
+
+        public bool IsHot
+        {
+            get { return _isHot; }
+            set
+            {
+                var changed = value != _isHot;
+                _isHot = value;
+                if (changed) OnPropertyChanged("IsHot");
+            }
+        }
+
+        public ICommand StopCommand()
+        {
+            return new RelayCommand(param => Stop());
+        }
+
+        public ICommand ReadCommand()
+        {
+            return new RelayCommand(param => Read(), param => !IsReading);
+        }
+
+        public ICommand ClearCommand()
+        {
+            return new RelayCommand(param => Clear());
+        }
+
+        private void Stop()
+        {
+            IsReading = false;
+        }
+
+        private void Clear()
+        {
+            // empty the local storage collection
+            //Values.Clear();
+        }
+
+        private void Read()
+        {
+            IsReading = true;
+
+            Action readFromTread = () =>
+            {
+                while (IsReading)
+                {
+                    Thread.Sleep(100);
+                    var r = new Random();
+                    _trend += (r.NextDouble() < 0.5 ? 1 : -1) * r.Next(0, 10) * .001;
+
+                    IsHot = _trend > 0;
+                    CurrentLecture = _trend;
+                }
+            };
+            
+            Task.Factory.StartNew(readFromTread);
+            //Task.Factory.StartNew(readFromTread);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
